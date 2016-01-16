8004fced43d728df4852c6b6d256e313b32dfa76
Aapo Hanski <aapo.hanski@gmail.com>
2016-01-16 16:33:29 +0200

    database for the images (migrations needed)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const uuid = require('node-uuid');
+const sqlite3 = require('sqlite3');
 
 const app = express();
 
@@ -11,7 +12,7 @@ const acceptedMIMETypes = {
 };
 
 function fileFilter(req, file, cb) {
-    cb(null, acceptedMIMETypes.indexOf(file.mimetype.keys()) !== -1);
+    cb(null, Object.keys(acceptedMIMETypes).indexOf(file.mimetype) !== -1);
 }
 
 const storage = multer.diskStorage({
@@ -29,7 +30,44 @@ const storage = multer.diskStorage({
 const upload = multer({ storage, fileFilter });
 
 app.post('/rest/images', upload.single('image'), function(req, res) {
-    res.json(req.body);
+    const db = new sqlite3.Database('database.sqlite3');
+
+    const today = new Date();
+    const todayISO = today.toISOString().slice(0, 10);
+
+    db.run(`INSERT INTO images (id, title, description, uploaded)
+           VALUES ($id, $title, $description, $uploaded)`, {
+               $id: req.body.id,
+               $title: req.body.title,
+               $description: req.body.description,
+               $uploaded: todayISO
+           });
+
+    db.close(function() {
+        res.json({
+            id: req.body.id,
+            title: req.body.title,
+            description: req.body.description,
+            uploadsed: todayISO
+        });
+    });
+});
+
+app.get('/rest/images/:id', function(req, res) {
+    const db = new sqlite3.Database('database.sqlite3');
+
+    db.get('SELECT * FROM images WHERE id = ?', req.params.id, function(err, row) {
+        Object.assign(row, { url: `/${row.id}.jpg` });
+        res.json(row);
+    });
+});
+
+app.get('/rest/images', function(req, res) {
+    const db = new sqlite3.Database('database.sqlite3');
+
+    db.all('SELECT * FROM images', req.params.id, function(err, rows) {
+        res.json(rows.map(row => Object.assign(row, { url: `/${row.id}.jpg` })));
+    });
 });
 
 app.use(express.static('static'));
