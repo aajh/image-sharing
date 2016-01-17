dc9569104fa213868c74a34e0cb845d35418840a
Aapo Hanski <aapo.hanski@gmail.com>
2016-01-17 14:18:27 +0200

    Test DB data

---
+++ migrations/test/sqls/20160117114616-test-images-up.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO images
+       SELECT '26f1d2ea-bd12-11e5-9912-ba0be0483c18' AS id, '2015-12-29 14:00:46' AS uploaded, 'Some flowers' AS title, 'Phasellus pulvinar pharetra odio imperdiet tempus.' AS description
+UNION ALL SELECT '26f1d718-bd12-11e5-9912-ba0be0483c18', '2016-01-10 12:43:13', 'Red-green plants', 'Vivamus faucibus fringilla diam, vel aliquam tortor aliquam ut.'
+UNION ALL SELECT '26f1d81c-bd12-11e5-9912-ba0be0483c18', '2016-01-11 18:27:54', 'Building', 'Donec consectetur ac arcu nec vulputate'
+UNION ALL SELECT '26f1d98e-bd12-11e5-9912-ba0be0483c18', '2016-01-11 19:21:42', 'Lamps', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+UNION ALL SELECT '26f1dc2c-bd12-11e5-9912-ba0be0483c18', '2016-01-12 09:14:07', 'Orange', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
