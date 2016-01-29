2d44ac82f41a3196936e92569a572d3317a20a0d
Aapo Hanski <aapo.hanski@gmail.com>
2016-01-29 20:32:06 +0200

    Made server.js compatible with older Javascript versions

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const multer = require('multer');
-const uuid = require('node-uuid');
-const sqlite3 = require('sqlite3');
-const Promise = require('bluebird');
-const glob = Promise.promisify(require('glob'));
-const path = require('path');
+"use strict";
+var express = require('express');
+var multer = require('multer');
+var uuid = require('node-uuid');
+var sqlite3 = require('sqlite3');
+var Promise = require('bluebird');
+var glob = Promise.promisify(require('glob'));
+var fs = Promise.promisifyAll(require('fs'));
+var path = require('path');
 
-const db = Promise.promisifyAll(new sqlite3.Database('dev.db'));
-const app = express();
+var db = Promise.promisifyAll(new sqlite3.Database('dev.db'));
+var app = express();
 
 
 
-const acceptedMIMETypes = {
+var acceptedMIMETypes = {
     "image/jpeg": ".jpg",
     "image/png": ".png",
     "image/gif": ".gif"
@@ -21,38 +23,45 @@ function fileFilter(req, file, cb) {
     cb(null, Object.keys(acceptedMIMETypes).indexOf(file.mimetype) !== -1);
 }
 
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, 'uploads/');
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        fs.mkdirAsync(path.join(__dirname + '/uploads/')) // Create dir
+          .catch(Error, err => {
+              if (err.code === 'EEXIST') return;
+              else throw err;
+          }).then(function() { cb(null, 'uploads/'); });
     },
-    filename(req, file, cb) {
-        const extension = acceptedMIMETypes[file.mimetype];
-        const id = uuid.v1();
+    filename: function(req, file, cb) {
+        var extension = acceptedMIMETypes[file.mimetype];
+        var id = uuid.v1();
         req.body.id = id;
         cb(null, id + extension);
     }
 });
 
-const upload = multer({ storage, fileFilter });
+var upload = multer({ storage: storage, fileFilter: fileFilter });
 
 
 
-class NotFoundError extends Error {
+function NotFoundError(message)  {
+    this.name = "NotFoundError";
+    this.message = message || "";
 }
+NotFoundError.prototype = Error.prototype;
 
 function fixTimestamp(timestamp) {
     return timestamp.split(' ').join('T');
 }
 
 function getImageWithSrc(image) {
-    return glob(`${__dirname}/uploads/${image.id}.*`)
-        .then(paths => {
+    return glob(__dirname + '/uploads/' + image.id + '.*')
+        .then(function(paths) {
             if (paths.length === 0) {
-                throw new NotFoundError(`Image file for image_id ${image.id} not found.`);
+                throw new NotFoundError('Image file for image_id '+ image.id +'not found.');
             }
-            const ext = path.extname(paths[0]);
-            const uploaded = fixTimestamp(image.uploaded);
-            return Object.assign({}, image, { src: `/${image.id}${ext}`, uploaded });
+            var ext = path.extname(paths[0]);
+            var uploaded = fixTimestamp(image.uploaded);
+            return Object.assign({}, image, { src: '/' + image.id + ext, uploaded });
         });
 }
 
@@ -68,13 +77,15 @@ function checkIfFound(a) {
 
 function getComments(image_id) {
     return db.allAsync('SELECT * FROM comments WHERE image_id = ?', image_id)
-        .map(c => Object.assign({}, c, { timestamp: fixTimestamp(c.timestamp)}));
+        .map(function(c) {
+            return Object.assign({}, c, { timestamp: fixTimestamp(c.timestamp)});
+        });
 }
 
 
 
 
-app.post('/rest/images', upload.single('image'), (req, res) => {
+app.post('/rest/images', upload.single('image'), function(req, res) {
     db.runAsync(`INSERT INTO images (id, title, description)
            VALUES ($id, $title, $description)`, {
                $id: req.body.id,
@@ -88,29 +99,29 @@ app.post('/rest/images', upload.single('image'), (req, res) => {
       .catch(err => res.status(500).end());
 });
 
-app.get('/rest/images', (req, res) => {
+app.get('/rest/images', function(req, res) {
     db.allAsync('SELECT * FROM images', req.params.id)
       .map(getImageWithSrc)
-      .then(images => res.json(images))
-      .catch(err => res.status(500).end());
+      .then(function(images) { return res.json(images); })
+      .catch(function(err) { return res.status(500).end(); });
 });
 
-app.get('/rest/images/:id', (req, res) => {
+app.get('/rest/images/:id', function(req, res) {
     Promise.join(db.getAsync('SELECT * FROM images WHERE id = ?', req.params.id)
                    .then(checkIfFound)
                    .then(getImageWithSrc),
                  getComments(req.params.id),
-                 (image, comments) => {
+                 function(image, comments) {
                      Object.assign(image, { comments });
                      res.json(image);
                  })
-           .catch(NotFoundError, err => res.status(404).end())
-           .catch(err => res.status(500).end());
+           .catch(NotFoundError, function(err) { res.status(404).end(); })
+           .catch(function(err) { res.status(500).end(); });
 });
 
 
 
-app.post('/rest/images/:image_id/comments', (req, res) => {
+app.post('/rest/images/:image_id/comments', function(req, res) {
     if (req.query.username === '' || req.query.comment === '') {
         return res.status(400).end();
     }
@@ -120,21 +131,21 @@ app.post('/rest/images/:image_id/comments', (req, res) => {
                 $username: req.query.username,
                 $comment: req.query.comment
             })
-      .then(() => getComments(req.params.image_id))
-      .then(comments => res.json(comments))
-      .catch(err => res.status(500).end());
+      .then(function() { return getComments(req.params.image_id); })
+      .then(function(comments) { return res.json(comments); })
+      .catch(function(err) { res.status(500).end(); });
 });
 
-app.get('/rest/images/:image_id/comments', (req, res) => {
+app.get('/rest/images/:image_id/comments', function(req, res) {
     getComments(req.params.image_id)
-        .then(comments => res.json(comments))
-        .catch(err => res.status(500).end());
+        .then(function(comments) { return res.json(comments); })
+        .catch(function(err) { res.status(500).end(); });
 });
 
 
 
 app.use(express.static('static'));
 app.use(express.static('uploads'));
-app.get('*', (req, res) => res.sendFile(`${__dirname}/static/index.html`));
+app.get('*', function(req, res) { res.sendFile(`${__dirname}/static/index.html`) });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, function() { console.log('Listening on port 3000!'); });
